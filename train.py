986a3fde92c5984b3aed49dbb0c0f5f7737ae84a
Swathi <71275010+mylearnings1@users.noreply.github.com>
2022-04-28 11:16:38 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,7 +1,51 @@
-X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=1)
+import pandas as pd
+import numpy as np
+from sklearn.cross_validation import train_test_split
+from sklearn import metrics #for checking the model accuracy
+from sklearn.tree import DecisionTreeClassifier
+import valohai
     
-    X_train = pd.DataFrame(X_train)
-    X_test=pd.DataFrame(X_test)
-    y_train = pd.DataFrame(y_train)
-    y_test=pd.DataFrame(y_test)
+def main():
+    
+    valohai.prepare(
+        step = 'Train-Model',
+        image = 'valohai/notebook:sklearn-0.24.2',
+        default_inputs={
+            'dataset':
+        },
+    )
+    
+    iris=valohai.inputs('dataset').path()
+    iris=pd.read_csv(iris,index_col=0)
+    X=iris.drop(columns=['Species'],axis=1)
+    y=iris['Species']
+    
+    X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=1)
+    
+    #X_train = pd.DataFrame(X_train)
+    #X_test=pd.DataFrame(X_test)
+    #y_train = pd.DataFrame(y_train)
+    #y_test=pd.DataFrame(y_test)
+    
+    model=DecisionTreeClassifier()
+    model.fit(X_train,y_train)
+    prediction=model.predict(X_test)
+    model_accuracy=metrics.accuracy_score(prediction,y_test)
+    print('The accuracy of the Decision Tree is',model_accuracy)
+    
+    cm= confusion_matrix(y_test,prediction)
+    plt.rcParams['figure.figsize']=(5,5)
+    sns.set(style='dark',font_scale=1.4)
+    sns.heatmap(cm,annot=True,annot_kws={"size":15})
+    
+    suffix=uuid.uuid4()
+    save_path=valohai.outputs().path('confusion_matrix.png')
+    plt.savefig(save_path)
+    plt.show()
+    plt.close()
+    model_path=valohai.outputs().path('model_dt.jbl')
+    joblib.dump(moodel,open(model_path,'wb'))
+    
+if __name__=='__main__':
+    main()
     
