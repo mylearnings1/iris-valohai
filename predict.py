3164b731a6d3fc5a68ae81cd155e47b6e09e3ed3
Swathi <71275010+mylearnings1@users.noreply.github.com>
2022-04-28 16:24:08 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,64 +1,31 @@
-from werkzeug.wrappers import Request, Response
-import io
-import numpy
+from fastapi import FastAPI, File, UploadFile
+
+import sklearn
 import pandas as pd
-import sklearn 
-import json
+import numpy
+import joblib
+
+app = FastAPI()
 
 model_path = 'model_dt.jbl'
+loaded_model = None
 
-# Store our model
-IrisModel = None
+@app.post("{full_path:path}")
+async def predict(image: UploadFile = File(...)):
+    img = open(image.read()))
 
-def read_input(request):
-    # Ensure that we've received a file named 'data' through POST
-    # If we have a valid request proceed, otherwise return None
-    if request.method != 'POST' and 'data' not in request.files:
-        return None
-    
-    # Load the data that was sent
-    DataFile = request.files.get('data')
-    dt = open(DataFile.stream,w+)
-    dt.load()
+    # Resize image and convert to grayscale
+    #img = img.resize((28, 28)).convert('L')
+    #img_array = numpy.array(img)
 
-    # Resize image to 28x28 and convert to grayscale
-    #dt = dt.drop(columns=['Id'],axis=1)
+    #image_data = numpy.reshape(img_array, (1, 28, 28))
 
-    return dt
+    global loaded_model
+    # Check if model is already loaded
+    if not loaded_model:
+        loaded_model = joblib.load(model_path)
 
+    # Predict with the model
+    prediction = loaded_model.predict(img)
 
-def mypredictor(environ, start_response):
-    # Get the request object from the environment
-    request = Request(environ)
-
-    global IrisModel
-    if not IrisModel:
-        IrisModel = joblib.load(model_path)
-
-    # Get the image file from our request
-    data = read_input(request)
-
-    # If read_input didn't find a valid file
-    if (data is None):
-        response = Response("\nNo data", content_type='text/html')
-        return response(environ, start_response)
-
-    # Use our model to predict the class of the file sent over a form.
-    prediction = IrisModel.predict(data)
-
-    # Generate a JSON output with the prediction
-    json_response = json.dumps("{Predicted_Digit: %s}" % prediction)
-
-    # Send a response back with the prediction
-    response = Response(json_response, content_type='application/json')
-    return response(environ, start_response)
-
-# When running locally
-if __name__ == "__main__":
-    from werkzeug.serving import run_simple
-
-    # Update model path to point to our downloaded model when testing locally
-    model_path = '.models/model_dt.jbl'
-
-    # Run a local server on port 5000.
-    run_simple("localhost", 8000, mypredictor)
+    return f'Predicted_Flower: {prediction}'
