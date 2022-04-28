1faa0d475c906853d4e0427d66f8aa2f16d4b51e
Swathi <71275010+mylearnings1@users.noreply.github.com>
2022-04-28 11:17:24 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,8 +1,12 @@
+import uuid
 import pandas as pd
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 from sklearn.cross_validation import train_test_split
 from sklearn import metrics #for checking the model accuracy
 from sklearn.tree import DecisionTreeClassifier
+import joblib
 import valohai
     
 def main():
