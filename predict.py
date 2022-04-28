3d5b0e50f8b0b16007241969cd71f7b881759d59
Swathi <71275010+mylearnings1@users.noreply.github.com>
2022-04-28 16:34:33 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,31 +1,38 @@
-from fastapi import FastAPI, File, UploadFile
-
-import sklearn
+import json
+from werkzeug.wrappers import Request, Response
 import pandas as pd
-import numpy
 import joblib
 
-app = FastAPI()
+model = None
 
-model_path = 'model_dt.jbl'
-loaded_model = None
+def predict(environ, start_response):
+    # Load input image data from the HTTP request
+    request = Request(environ)
+    if not request.files:
+        return Response('no file uploaded', 400)(environ, start_response)
+    data_file = next(request.files.values())
+    #data = process_image(Image.open(image_file))
+    data = pd.read_csv(data_file)
 
-@app.post("{full_path:path}")
-async def predict(image: UploadFile = File(...)):
-    img = open(image.read()))
+    # The predictor must be lazily instantiated;
+    # the TensorFlow graph can apparently not be shared
+    # between processes.
+    global model
+    if not model:
+        model = joblib.load('model_dt.jbl')
+    prediction = model.predict(data)
 
-    # Resize image and convert to grayscale
-    #img = img.resize((28, 28)).convert('L')
-    #img_array = numpy.array(img)
+    # The following line allows Valohai to track endpoint predictions
+    # while the model is deployed. Here we remove the full predictions
+    # details as we are only interested in tracking the rest of the results.
+    #print(json.dumps({'vh_metadata': {k: v for k, v in prediction.items() if k != 'predictions'}}))
 
-    #image_data = numpy.reshape(img_array, (1, 28, 28))
+    # Return a JSON response
+    response = Response(json.dumps(prediction), content_type='application/json')
+    return response(environ, start_response)
 
-    global loaded_model
-    # Check if model is already loaded
-    if not loaded_model:
-        loaded_model = joblib.load(model_path)
 
-    # Predict with the model
-    prediction = loaded_model.predict(img)
-
-    return f'Predicted_Flower: {prediction}'
+# Run a local server for testing with `python deploy.py`
+if __name__ == '__main__':
+    from werkzeug.serving import run_simple
+    run_simple('0.0.0.0', 8000, predict)
