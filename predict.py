a2de68b8f1dc8328715fcda8165180b1016eebf7
Swathi <71275010+mylearnings1@users.noreply.github.com>
2022-04-28 14:56:44 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,1 +1,17 @@
+from werkzeug.wrappers import Request, Response
+import io
+import numpy
+import pandas as pd
+import sklearn 
+import json
 
+model_path = 'model_dt.jbl'
+
+# Store our model
+IrisModel = None
+
+def read_input(request):
+    # Ensure that we've received a file named 'image' through POST
+    # If we have a valid request proceed, otherwise return None
+    if request.method != 'POST' and 'image' not in request.files:
+        return None
