fe0446a083c71069158ccae2e216bdbff0196864
Swathi <71275010+mylearnings1@users.noreply.github.com>
2022-04-28 15:28:34 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -11,7 +11,54 @@ model_path = 'model_dt.jbl'
 IrisModel = None
 
 def read_input(request):
-    # Ensure that we've received a file named 'image' through POST
+    # Ensure that we've received a file named 'data' through POST
     # If we have a valid request proceed, otherwise return None
-    if request.method != 'POST' and 'image' not in request.files:
+    if request.method != 'POST' and 'data' not in request.files:
         return None
+    
+    # Load the data that was sent
+    DataFile = request.files.get('data')
+    dt = open(DataFile.stream,w+)
+    dt.load()
+
+    # Resize image to 28x28 and convert to grayscale
+    dt = dt.drop(columns=['Id'],axis=1)
+
+    return dt
+
+
+def mypredictor(environ, start_response):
+    # Get the request object from the environment
+    request = Request(environ)
+
+    global IrisModel
+    if not IrisModel:
+        IrisModel = joblib.load(model_path)
+
+    # Get the image file from our request
+    data = read_input(request)
+
+    # If read_input didn't find a valid file
+    if (data is None):
+        response = Response("\nNo data", content_type='text/html')
+        return response(environ, start_response)
+
+    # Use our model to predict the class of the file sent over a form.
+    prediction = IrisModel.predict(data)
+
+    # Generate a JSON output with the prediction
+    json_response = json.dumps("{Predicted_Digit: %s}" % prediction[0])
+
+    # Send a response back with the prediction
+    response = Response(json_response, content_type='application/json')
+    return response(environ, start_response)
+
+# When running locally
+if __name__ == "__main__":
+    from werkzeug.serving import run_simple
+
+    # Update model path to point to our downloaded model when testing locally
+    model_path = '.models/model_dt.jbl'
+
+    # Run a local server on port 5000.
+    run_simple("localhost", 8000, mypredictor)
