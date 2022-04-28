2594e8a2c60fe545e534db69ea8730fb73814091
Swathi <71275010+mylearnings1@users.noreply.github.com>
2022-04-28 11:00:57 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,1 +1,7 @@
-
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=1)
+    
+    X_train = pd.DataFrame(X_train)
+    X_test=pd.DataFrame(X_test)
+    y_train = pd.DataFrame(y_train)
+    y_test=pd.DataFrame(y_test)
+    
